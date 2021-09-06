300b2268ddee704ddf8fb366cf562bf65d82f61e
genesissiana <perezgenesisa@gmail.com>
2021-09-06 12:48:27 -0700

    updated server and idb file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,36 @@
-const APP_PREFIX = 'BudgetTracker-';
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./js/index.js",
-  "./js/idb.js",
-  "./assets/css/styles.css",
-  "./assets/icons/icon-72x72.png",
-  "./assets/icons/icon-96x96.png",
-  "./assets/icons/icon-128x128.png",
-  "./assets/icons/icon-144x144.png",
-  "./assets/icons/icon-152x152.png",
-  "./assets/icons/icon-192x192.png",
-  "./assets/icons/icon-384x384.png",
-  "./assets/icons/icon-512x512.png"
-]
+  '/',
+  './index.html',
+  './assets/js/index.js',
+  './assets/js/idb.js',
+  './manifest.json',
+  './assets/css/styles.css',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'  
+];
 
-self.addEventListener('install', function(e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE)
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
     })
-  )
+  );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
-    caches.keys().then(function(keyList) {
+    caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
@@ -38,6 +39,7 @@ self.addEventListener('activate', function(e) {
       return Promise.all(
         keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
@@ -46,14 +48,17 @@ self.addEventListener('activate', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
+        console.log("responding with cache : " + e.request.url);
         return request;
       } else {
-        return fetch(e.request)
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
       }
     })
-  )
-})
+  );
+});
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 3002;
+
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
