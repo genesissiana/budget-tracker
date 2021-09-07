10561f9e5861f9cf17a36ffc3375881fbf13f748
genesissiana <perezgenesisa@gmail.com>
2021-09-07 10:02:08 -0700

    Deploying

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//updated in heroku
 mongoose.connect(MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
